c8cf81acd579fef3c155f77af87a632d82a472f6
cheehoong <lechho@gmail.com>
2021-12-13 23:23:13 +0800

    Initial commit

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import os
 import sys
 import time
 import requests
-
+import logging
 
 picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic/2in13')
 fontdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
@@ -17,16 +17,23 @@ from python.lib import gt1151
 from python.lib import epd2in13_V2
 from PIL import Image, ImageDraw, ImageFont
 
+logging.basicConfig(level=logging.DEBUG)
+flag_t = 1
+
+logging.info("1")
 response2 = requests.get("http://volumio0.local:3000/api/v1/getState")
 r2 = response2.json()
 
+logging.info("epd2in13_V2 Touch Demo")
 epd = epd2in13_V2.EPD_2IN13_V2()
 gt = gt1151.GT1151()
 GT_Dev = gt1151.GT_Development()
 GT_Old = gt1151.GT_Development()
 
+logging.info("clear the frame")
 image = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
 
+logging.info("draw")
 draw = ImageDraw.Draw(image)
 font = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
 draw.rectangle((0, 10, 200, 34), fill=0)
