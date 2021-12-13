d512e89973fbd44b5a74c8abd754f0c0c875bd0c
cheehoong <lechho@gmail.com>
2021-12-13 23:50:43 +0800

    Initial commit

--- test.py
+++ test.py
@@ -10,6 +10,10 @@ from python.lib import epd2in13_V2
 from PIL import Image, ImageDraw, ImageFont
 
 picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic/2in13')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
 font = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
 
 logging.basicConfig(level=logging.DEBUG)
