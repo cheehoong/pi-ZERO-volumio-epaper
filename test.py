c7048bd4d6cf3c4b60006f139e21f811a48668ca
cheehoong <lechho@gmail.com>
2021-12-13 23:44:25 +0800

    Initial commit

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ import os
 import time
 import requests
 import logging
-import gt1151
-import epd2in13_V2
+from python.lib import gt1151
+from python.lib import epd2in13_V2
 from PIL import Image, ImageDraw, ImageFont
 
 picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic/2in13')
