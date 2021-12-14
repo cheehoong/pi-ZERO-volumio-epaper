03274094989b33d7aceb5fb162a6835fc4a03fab
cheehoong <lechho@gmail.com>
2021-12-15 01:05:24 +0800

    Initial commit

--- test.py
+++ test.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
-import os
-import sys
-import time
-import requests
 import logging
+import os
+import time
+
+import requests
 from PIL import Image, ImageDraw, ImageFont
-from .lib import epd2in13_V2
-from .lib import gt1151
+
+import epd2in13_V2
+import gt1151
 from TP2in13_test import picdir
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'lib')))
+
 font = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
 
 logging.basicConfig(level=logging.DEBUG)
