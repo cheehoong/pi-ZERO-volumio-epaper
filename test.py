8e0ecb1d469d20e120b33317e1284c55187a451f
cheehoong <lechho@gmail.com>
2021-12-18 01:20:06 +0800

    Initial commit

--- test.py
+++ test.py
@@ -4,9 +4,13 @@ import argparse
 import os
 import logging
 import time
+
+import yaml
+
 from libz import epd2in13_V2
 from libz import gt1151
 from PIL import Image, ImageDraw, ImageFont
+from socketIO_client import SocketIO
 import requests
 
 logging.basicConfig(level=logging.DEBUG)
@@ -16,8 +20,6 @@ picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')  # Poi
 fontdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'fonts')
 
 logging.info("Start initial")
-
-logging.info("epd2in13_V2 Touch Demo")
 epd = epd2in13_V2.EPD_2IN13_V2()
 gt = gt1151.GT1151()
 GT_Dev = gt1151.GT_Development()
@@ -27,9 +29,6 @@ logging.info("init and Clear")
 epd.init(epd.FULL_UPDATE)
 gt.GT_Init()
 
-def clear_display(display):
-    logging.info('Clearing display...')
-    epd.Clear(0xFF)
 
 def parse_args():
     p = argparse.ArgumentParser(description='Test EPD functionality')
@@ -43,6 +42,7 @@ def parse_args():
                    help='Brings up the error screen for formatting')
 
     return p.parse_args()
+
 
 def on_connect():
     logging.info('connect')
@@ -61,91 +61,35 @@ def on_push_state(*args):
     ismuted = bool(args[0]['volume'] < mutethresh)
     wasplaying = bool(lastpass['status'] == 'play')
     isplaying = bool(args[0]['status'] == 'play')
-    techstring = False
     if ((args[0]['title'] != lastpass['title'] or (wasplaying != isplaying)) and args[0]['status'] != 'stop') or \
             wasmuted != ismuted:
         lastpass = args[0]
-        img = Image.new('RGBA', (display.width, display.height), color=(255, 255, 255, 0))
-
-        # Load cover
-        albumart = args[0]['albumart'].encode('ascii', 'ignore')
-
-        # Make the album art link into a url & get it & paste it in
-        if b'http://' in albumart:
-            # It's already a url, which means radio, no need to do anything
-            pass
-        else:
-            # Otherwise turn it into a url
-            albumart = 'http://' + servername + ':3000' + args[0]['albumart']
-        try:
-            response = requests.get(albumart)
-            imgart = Image.open(BytesIO(response.content))
-            imgart = imgart.convert("RGBA")
-            imgart = imgart.resize((coversize, coversize))
-        except:
-            # If there are issues getting the icon, just use a blank space
-            imgart = Image.new('RGBA', (coversize, coversize), color=(255, 255, 255, 0))
-
-        img.paste(imgart, (int((display.width - coversize) / 2), 220), imgart)
-
+        img = Image.open(os.path.join(picdir, 'Empty2.bmp'))
+        draw = ImageDraw.Draw(img)
         if args[0]['status'] in ['pause', 'stop']:
-            img.paste(pause_icons, (indent, 300), pause_icons)
-
-        draw = ImageDraw.Draw(img, 'RGBA')
-        fontsize = 80
-        y_text = -450
-        height = 80
-        width = 25
+            draw.text((8, 70), 'pause', font=font15, fill=0)
         if 'artist' in args[0]:
-            img, numline = writewrappedlines(img, args[0]['artist'], fontsize, y_text, height, width, fontstring)
-        y_text = 160
-        height = 50
-        fontsize = 50
-        width = 40
+            draw.text((8, 50), 'by : ' + info['artist'], font=font15, fill=0)
         if 'album' in args[0] and args[0]['album'] is not None:
-            titletext = smart_truncate(args[0]['album'], length=27)
-            img, numline = writewrappedlines(img, args[0]['album'], fontsize, y_text, height, width, fontstring)
-
-        if 'stream' in args[0]:
-            if str(args[0]['stream']) is 'True':
-                techstring = "(" + str(args[0]['bitrate'])
-            else:
-                techstring = "(" + str(args[0]['stream'])
-            techflag = True
-        if 'samplerate' in args[0] and args[0]['samplerate'] != '':
-            techstring += ", " + str(args[0]['samplerate'])
-        if 'bitdepth' in args[0] and args[0]['bitdepth'] != '':
-            techstring += ", " + str(args[0]['bitdepth'])
-        if techflag:
-            y_text = 210
-            fontsize = 40
-            img, numline = writewrappedlines(img, techstring + ")", fontsize, y_text, height, width, fontstring)
-        y_text = 290
-        fontsize = 120
-        height = 120
-        width = 20
+            draw.text((8, 30), 'Album : ' + info['album'], font=font15, fill=0)
         if 'title' in args[0] and args[0]['title'] is not None:
-            titletext = smart_truncate(args[0]['title'], length=33)
-            img, numline = writewrappedlines(img, titletext, fontsize, y_text, height, width, fontstring)
+            draw.text((8, 10), 'Song : ' + info['title'], font=font15, fill=0)
 
         vol_x = int(float(args[0]['volume']))
 
         if vol_x <= mutethresh:
             logging.info('muted')
-            img.paste(mute_icons, (iconheight, 300), mute_icons)
-        display_image_8bpp(display, img)
+            draw.text((38, 70), 'muted', font=font15, fill=0)
+        im2 = img.transpose(method=Image.ROTATE_90)
+        image.paste(im2, (2, 2))
+        epd.displayPartial(epd.getbuffer(im2))
     return
 
-def smart_truncate(content, length=100, suffix='...'):
-    if len(content) <= length:
-        return content
-    else:
-        return ' '.join(content[:length+1].split(' ')[0:-1]) + suffix
 
 # get the path of the script
 script_path = os.path.dirname(os.path.abspath(__file__))
 dirname = os.path.dirname(__file__)
-configfile = os.path.join(os.path.dirname(os.path.realpath(__file__)),'config.yaml')
+configfile = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.yaml')
 
 # set script path as current directory
 os.chdir(script_path)
@@ -170,6 +114,32 @@ if not args.virtual:
 else:
     from IT8951.display import VirtualEPDDisplay
     display = VirtualEPDDisplay(dims=(1448, 1072), rotate=args.rotate)
+
+# Initialise some constants
+
+rabbit_icon = Image.open('pic/rabbitsq.png').resize((300, 300)).convert("RGBA")
+pause_icons = Image.open('pic/pause.png').resize((240, 240)).convert("RGBA")
+mute_icons = Image.open('pic/mute.png').resize((240, 240)).convert("RGBA")
+
+coversize = config['display']['coversize']
+mutethresh = 1
+indent = config['display']['indent']
+servername = config['server']['name']
+fontstring = config['display']['font']
+
+# Derive some constants
+iconheight = display.width-240-indent
+socketIO = SocketIO(servername, 3000)
+socketIO.on('connect', on_connect)
+lastpass = {
+  "artist": "none",
+  "title": "none",
+  "album": "none",
+  "albumart": "none",
+  "status": "none",
+  "volume": 60
+}
+
 
 # Drawing on the image
 font15 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 15)
@@ -196,13 +166,30 @@ draw = ImageDraw.Draw(im)
 # draw.rectangle((0, 10, 20, 34), fill=0)
 # draw.line((16, 60, 56, 60), fill=0)
 logging.info("drawline")
-draw.text((8, 10), 'Song : ' + info['title'], font=font15, fill=0)
-draw.text((8, 30), 'Album : ' + info['album'], font=font15, fill=0)
-draw.text((8, 50), 'by : ' + info['artist'], font=font15, fill=0)
 im2 = im.transpose(method=Image.ROTATE_90)
 image.paste(im2, (2, 2))
 epd.displayPartial(epd.getbuffer(im2))
 epd.init(epd.PART_UPDATE)
 
-time.sleep(2)
 
+def main():
+    while True:
+        # connecting to socket
+        socketIO.on('pushState', on_push_state)
+        # get initial state
+        socketIO.emit('getState', '', on_push_state)
+        # now wait
+        socketIO.wait()
+        logging.info('Reconnection needed')
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        socketIO.disconnect()
+        img = Image.open(os.path.join(picdir, 'Empty2.bmp'))
+        img.paste(rabbit_icon, (80, 80), rabbit_icon)
+        epd.displayPartial(epd.getbuffer(img))
+        pass
