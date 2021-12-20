3561c81c5a570c3b78e87654f7494b1df0b03db7
cheehoong <lechho@gmail.com>
2021-12-20 23:38:49 +0800

    Initial commit

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ fontdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'fonts')
 font15 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 15)
 font20 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 20)
 rabbit_icon = Image.open(os.path.join(picdir, 'rabbitsq.png')).resize((30, 30)).convert(0)
+baseimage = os.path.join(picdir, 'Empty2.bmp')
 
 # Read config setting
 config = ConfigParser()
@@ -41,7 +42,7 @@ logging.info("init and Clear")
 epd.init(epd.FULL_UPDATE)
 gt.GT_Init()
 
-image = Image.open(os.path.join(picdir, 'Empty2.bmp'))
+image = Image.open(baseimage)
 epd.displayPartBaseImage(epd.getbuffer(image))
 DrawImage = ImageDraw.Draw(image)
 epd.init(epd.PART_UPDATE)
@@ -90,8 +91,10 @@ def on_push_state(*args):
     print('Artist = ' + artist)
     print('Status = ' + status)
     lastpass = args[0]
-    img = Image.open(os.path.join(picdir, 'Empty2.bmp'))
-    draw = ImageDraw.Draw(img)
+    img_d = Image.open(baseimage)
+#    img_c = cv2.imread(baseimage, 0)
+#    img_d = Image.fromarray(img_c)
+    draw = ImageDraw.Draw(img_d)
     if args[0]['status'] in ['pause', 'stop']:
         draw.text((8, 70), 'pause', font=font20, fill=0)
         draw.text((8, 90), status, font=font20, fill=0)
@@ -105,15 +108,10 @@ def on_push_state(*args):
         logging.info('muted')
         draw.text((38, 70), 'muted', font=font20, fill=0)
     im2 = img.transpose(method=Image.ROTATE_90)
-    img.paste(im2, (2, 2))
+    img_d.paste(im2, (2, 2))
     epd.displayPartial(epd.getbuffer(im2))
     epd.init(epd.PART_UPDATE)
     return
-
-
-
-
-
 
 
 def main():
