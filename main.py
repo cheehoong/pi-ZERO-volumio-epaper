b8ff28ed51909f98fb98a30ec14ca18174a9b2d4
cheehoong <lechho@gmail.com>
2021-12-25 09:04:00 +0800

    Initial commit

--- main.py
+++ main.py
@@ -165,7 +165,7 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         socketIO.disconnect()
         img = Image.open(os.path.join(picdir, 'Empty2.bmp'))
+        img.paste(rabbit_icon, (30, 100))
         imge = img.transpose(method=Image.ROTATE_90)
-        imge.paste(rabbit_icon, (30, 100))
         epd.displayPartial(epd.getbuffer(imge))
         pass
