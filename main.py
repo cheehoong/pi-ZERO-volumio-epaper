eb46ab6d8ba4cf008e6f8d7b6b761bc5cc64557e
cheehoong <lechho@gmail.com>
2021-12-25 23:38:15 +0800

    Initial commit

--- main.py
+++ main.py
@@ -2,9 +2,8 @@
 # -*- coding:utf-8 -*-
 import logging
 import os
-import threading
 import time
-
+import threading
 from configparser import ConfigParser
 from PIL import Image, ImageDraw, ImageFont
 from six import unichr
@@ -12,7 +11,7 @@ from socketIO_client import SocketIO
 from libz import epd2in13_V2
 from libz import gt1151
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 # get the path of the script
 file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.ini')
@@ -29,7 +28,6 @@ baseimage = os.path.join(picdir, 'Empty2.bmp')
 # Read config setting
 config = ConfigParser()
 config.read(file)
-logging.info('Read config setting')
 logging.info(config.sections())
 volumio_host = config.get('volumio', 'volumio_host')
 volumio_port = config.getint('volumio', 'volumio_port')
@@ -52,7 +50,24 @@ epd.init(epd.PART_UPDATE)
 
 flag_t = 1
 
+
+def pthread_irq():
+    print("pthread running")
+    while flag_t == 1:
+        if gt.digital_read(gt.INT) == 0:
+            GT_Dev.Touch = 1
+        else:
+            GT_Dev.Touch = 0
+    print("thread:exit")
+
+
+t = threading.Thread(target=pthread_irq)
+# t.setDaemon(True)
+t.daemon = True
+t.start()
+
 # Derive some constants
+socketIO = SocketIO(volumio_host, volumio_port)
 lastpass = {
     "artist": "none",
     "title": "none",
@@ -68,6 +83,58 @@ icon_play = unichr(0xF000 + 0x34)
 icon_pause = unichr(0xF000 + 0x3B)
 icon_stop = unichr(0xF000 + 0x3C)
 icon_sound = unichr(0xF000 + 0x58)
+
+
+def on_connect():
+    logging.info('connect')
+    return 'connected'
+
+
+def on_push_state(*args):
+    global lastpass
+    icon_status = icon_stop
+    # Only run screen update if the key arguments have changed since the last call. Key arguments are:
+    # status
+    # albumart
+    # artist, album, title
+    # Volume crosses mute threshold
+    artist = str(args[0]['artist'])
+    title = str(args[0]['title'])
+    album = str(args[0]['album'])
+    status = str(args[0]['status'])
+    vol_x = int(float(args[0]['volume']))
+    logging.info('Artist = ' + artist)
+    logging.info('Status = ' + status)
+    lastpass = args[0]
+    img_d = Image.open(baseimage)
+    #    img_c = cv2.imread(baseimage, 0)
+    #    img_d = Image.fromarray(img_c)
+    draw = ImageDraw.Draw(img_d)
+    if args[0]['status'] in ['pause', 'stop']:
+        if status == 'pause':
+            icon_status = icon_pause
+        if status == 'stop':
+            icon_status = icon_stop
+    else:
+        icon_status = icon_play
+    draw.text((120, 100), icon_status, font=font0w, fill=0)
+    if 'artist' in args[0]:
+        draw.text((8, 50), icon_artist, font=font0w, fill=0)
+        draw.text((28, 50), lastpass['artist'], font=font18, fill=0)
+    if 'album' in args[0] and args[0]['album'] is not None:
+        draw.text((8, 30), icon_album, font=font0w, fill=0)
+        draw.text((28, 30), lastpass['album'], font=font18, fill=0)
+    if 'title' in args[0] and args[0]['title'] is not None:
+        draw.text((8, 10), icon_song, font=font0w, fill=0)
+        draw.text((28, 10), lastpass['title'], font=font18, fill=0)
+    if vol_x <= 1:
+        logging.info('muted')
+        draw.text((38, 70), 'muted', font=font18, fill=0)
+    im2 = img_d.transpose(method=Image.ROTATE_90)
+    img_d.paste(im2, (2, 2))
+    epd.displayPartial(epd.getbuffer(im2))
+    epd.init(epd.PART_UPDATE)
+    return
 
 
 def touch():
@@ -86,100 +153,28 @@ def touch():
     print(GT_Old.S[0])
 
 
-def on_connect():
-    logging.info('connect')
-    return 'connected'
+def my_background_task():
+    socketIO.on('pushState', on_push_state)
+    socketIO.emit('getState', '', on_push_state)
+    socketIO.wait()
+    pass
 
 
-class TwoWayClient(object):
-    def on_event(self, event):
-        print('event')
-
-    def on_push_state(self, *args):
-        global lastpass
-        icon_status = icon_stop
-        # Only run screen update if the key arguments have changed since the last call. Key arguments are:
-        # status
-        # albumart
-        # artist, album, title
-        # Volume crosses mute threshold
-        artist = str(args[0]['artist'])
-        title = str(args[0]['title'])
-        album = str(args[0]['album'])
-        status = str(args[0]['status'])
-        vol_x = int(float(args[0]['volume']))
-        logging.info('Artist = ' + artist)
-        logging.info('Status = ' + status)
-        lastpass = args[0]
-        self.img_d = Image.open(baseimage)
-        #    img_c = cv2.imread(baseimage, 0)
-        #    img_d = Image.fromarray(img_c)
-        self.draw = ImageDraw.Draw(self.img_d)
-        if args[0]['status'] in ['pause', 'stop']:
-            if status == 'pause':
-                icon_status = icon_pause
-            if status == 'stop':
-                icon_status = icon_stop
-        else:
-            icon_status = icon_play
-        self.draw.text((120, 100), icon_status, font=font0w, fill=0)
-        if 'artist' in args[0]:
-            self.draw.text((8, 50), icon_artist, font=font0w, fill=0)
-            self.draw.text((28, 50), lastpass['artist'], font=font18, fill=0)
-        if 'album' in args[0] and args[0]['album'] is not None:
-            self.draw.text((8, 30), icon_album, font=font0w, fill=0)
-            self.draw.text((28, 30), lastpass['album'], font=font18, fill=0)
-        if 'title' in args[0] and args[0]['title'] is not None:
-            self.draw.text((8, 10), icon_song, font=font0w, fill=0)
-            self.draw.text((28, 10), lastpass['title'], font=font18, fill=0)
-        if vol_x <= 1:
-            logging.info('muted')
-            self.draw.text((38, 70), 'muted', font=font18, fill=0)
-        self.im2 = self.img_d.transpose(method=Image.ROTATE_90)
-        self.img_d.paste(self.im2, (2, 2))
-        epd.displayPartial(epd.getbuffer(self.im2))
-        epd.init(epd.PART_UPDATE)
-        return
-
-    def __init__(self):
-        # connecting to socket
-        self.im2 = None
-        self.draw = None
-        self.img_d = None
-        self.socketIO = SocketIO(volumio_host, volumio_port)
-        self.socketIO.on('pushState', self.on_push_state)
-
-        self.receive_events_thread = threading.Thread(target=self._receive_events_thread)
-        self.receive_events_thread.daemon = True
-        self.receive_events_thread.start()
-
-        while True:
-            # touch()
-            # get initial state
-            self.socketIO.emit('getState', '', self.on_push_state)
-
-    #            time.sleep(1)  # endless loop
-
-    def _receive_events_thread(self):
-        print("pthread running")
-        while flag_t == 1:
-            if gt.digital_read(gt.INT) == 0:
-                GT_Dev.Touch = 1
-            else:
-                GT_Dev.Touch = 0
-        print("thread:exit")
-        self.socketIO.wait()
+task = socketIO.start_background_task(target=my_background_task)
 
 
 def main():
-    TwoWayClient()
+    while True:
+        touch()
+        logging.info('Reconnection needed')
+        time.sleep(1)
 
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        TwoWayClient().socketIO.disconnect()
+        socketIO.disconnect()
         img = Image.open(os.path.join(picdir, 'Empty2.bmp'))
         img.paste(rabbit_icon, (80, 10))
         imge = img.transpose(method=Image.ROTATE_90)
