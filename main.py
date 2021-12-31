2e392cf1085da11545c7813aea2b96de224858df
cheehoong <lechho@gmail.com>
2021-12-31 20:45:08 +0800

    Initial commit

--- main.py
+++ main.py
@@ -203,6 +203,7 @@ def button_pressed(channel):
             volume_screen(lastpass['volume'])
         if page == 'volume_page':
             page = 'main_page'
+            main_screen()
     elif channel == 'touch_previous':
         print('previous')
         socketIO.emit('prev')
